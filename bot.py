4920a68483d59a779935f8c2fd36ccd0587ac806
Avedyyev <avedyyev.s@gmail.com>
2026-06-29 15:41:03 +0300

    Feat: implement revenue command handler using orders_manager logic

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 import asyncio
+from orders_manager import get_total_revenue
 
 load_dotenv()
 
@@ -14,6 +15,9 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def start_handler(message: types.Message):
     await message.answer("Welcome to Taxi Orders Manager!")
+@dp.message(Command("revenue"))
+async def revenue_handler(message: types.Message):
+    await message.answer(f"Выручка составляет {str(get_total_revenue())} руб.")
 
 async def main():
     await dp.start_polling(bot)
